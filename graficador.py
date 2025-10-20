d9b41b981c85aaa1c844f53146eb28fe1739754a
angelicaalvarez <angelica.alvarez@utp.edu.co>
2025-10-20 18:13:04 -0500

    ins

--- graficador.py
+++ graficador.py
@@ -1,1 +1,9 @@
 #santiago, angelica
+
+#para subir algo 1. commmit -> comentar que se hizo
+#2. push -> subir los cambios 
+# a veces pide sincronizar a veces no xd
+
+#para bajar cambios -> pull
+
+print ("Hola graficador")
