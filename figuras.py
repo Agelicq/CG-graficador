4cb4c371e8ef402b2576ce38b5e46f9e68fe1929
Santiago Ceballos <s.ceballos@utp.edu.co>
2025-10-22 16:58:03 -0500

    funciones restantes

--- figuras.py
+++ figuras.py
@@ -1,4 +1,5 @@
 import pygame 
+import math
 """
 PROBAR DDA Y BRESENHAM 
 import pygame
@@ -190,7 +191,41 @@ def bezier(screen, p0, p1, p2, p3, steps, color):
     # Dibuja la curva conectando los puntos
     for i in range(1, len(points)):
         pygame.draw.line(screen, color, points[i-1], points[i], 2)
+#--------------------------------------------------------------------------
+#Faltantes
+#Triangulos y Poligonos
+def draw_triangle(screen, vertices, color):
+    for i in range(len(vertices)):
+        x1, y1 = vertices[i]
+        x2, y2 = vertices[(i + 1) % len(vertices)]
+        dda(screen, x1, y1, x2, y2, color)
         
+def draw_polygon(screen, vertices, color):
+    for i in range(len(vertices)):
+        x1, y1 = vertices[i]
+        x2, y2 = vertices[(i + 1) % len(vertices)]
+        dda(screen, x1, y1, x2, y2, color)
+
+#--------------------------------------------------------------------------
+#Rectangulos
+def draw_rectangle(screen, x, y, width, height, color):
+    vertices = [
+        (x, y),
+        (x + width, y),
+        (x + width, y + height),
+        (x, y + height)
+    ]
+    draw_polygon(screen, vertices, color)  # Dibujar el rectangulo como un poligono
+
+#---------------------------------------------------------------------------
+#Elipsis
+def draw_ellipse(screen, cx, cy, rx, ry, color):
+    t_values = np.linspace(0, 2 * math.pi, 100)
+    for t in t_values:
+        x = cx + rx * math.cos(t)
+        y = cy + ry * math.sin(t)
+        pygame.draw.circle(screen, color, (int(x), int(y)), 1)  
+      
 import pygame
 import numpy as np
 
